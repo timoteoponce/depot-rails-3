9bede98a203c1d77660d277dfd36a24ad5e34c4d
Jon Homan <jon@jonhoman.com>
2011-02-12 20:45:54 -0600

    title has a minimum 10 characters

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -60,4 +60,13 @@ class ProductTest < ActiveSupport::TestCase
     assert_equal I18n.translate('activerecord.errors.messages.taken'),
       product.errors[:title].join('; ')
   end
+
+  test "product is not valid with title less than 10 characters" do
+    product = Product.new(:title => "short",
+                          :description => "yyy",
+                          :price => 1, 
+                          :image_url => "fred.gif")
+
+    assert product.invalid?
+  end
 end
